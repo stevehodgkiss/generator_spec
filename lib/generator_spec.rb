036ea55305d544d92f43f222831688a30b3627ab
Josef Šimánek <josef.simanek@gmail.com>
2013-07-10 16:08:13 +0200

    Generator example group and recognizing files under spec/lib/generators as generator example group.

---
+++ lib/generator_spec.rb
@@ -0,0 +1,12 @@
+require 'rspec'
+require 'generator_spec/generator_example_group'
+
+RSpec::configure do |c|
+  def c.escaped_path(*parts)
+    Regexp.compile(parts.join('[\\\/]') + '[\\\/]')
+  end
+
+  c.include GeneratorSpec::GeneratorExampleGroup, :type => :generator, :example_group => {
+    :file_path => c.escaped_path(%w[spec lib generators])
+  }
+end
